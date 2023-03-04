497c62d64a73719285238fac665dbac7801e9e39
Ismael267 <77760562+Ismael267@users.noreply.github.com>
2023-03-04 19:55:22 +0000

    ajout des commentaires

--- js/main.js
+++ js/main.js
@@ -1,24 +1,22 @@
-/*crud */
-$(function () {
-  let localuser = localStorage.getItem('result')
-  let result = JSON.parse(localuser).result
+/*crud */$(function () {
+  let localUserData = localStorage.getItem('result')
+  let userData = JSON.parse(localUserData).result
 
-  let token = result.token
-  let user_id = result.id
-  console.log(token)
-  console.log(user_id)
+  let userToken = userData.token
+  let userId = userData.id
+  console.log(userToken)
+  console.log(userId)
 
   /*lister*/
   $.ajax({
-    url: `http://greenvelvet.alwaysdata.net/bugTracker/api/list/${token}/${user_id}`,
+    url: `http://greenvelvet.alwaysdata.net/bugTracker/api/list/${userToken}/${userId}`,
     type: 'GET',
     success: function (response) {
       function appendRow(element) {
-        let users = localStorage.getItem('resultats')
-        let resultats = JSON.parse(users)
+        let localResultats = localStorage.getItem('resultats')
+        let resultats = JSON.parse(localResultats)
         let allUser = resultats.result.user
 
-        // console.log(allUser)
         const options = `
           <option id="option1" value="0">Pas commencé</option>
           <option id="option2" value="1">En cours</option>
@@ -37,25 +35,23 @@ $(function () {
             <td>${deleteButton}</td>
           </tr>
         `
-        // console.log(element.timestamp);
         $('#thead-dark').append(row)
       }
 
       try {
-        const datas = JSON.parse(response)
-        // console.log(datas)
-        const bug = datas.result.bug
-        console.log(bug)
+        const data = JSON.parse(response)
+        const bugList = data.result.bug
+        console.log(bugList)
 
-        const undone = bug.filter((b) => b.state == 1)
-        const dones = bug.filter((b) => b.state == 2)
-        const bug_length = bug.length
-        console.log(dones)
-        $('#all_bugs').append(bug_length)
-        $('#on_Bug').append(undone.length)
-        $('#done_bugs').append(dones.length)
-        localStorage.setItem('resultat', JSON.stringify(datas))
-        for (const element of datas.result.bug) {
+        const undoneBugs = bugList.filter((b) => b.state == 1)
+        const doneBugs = bugList.filter((b) => b.state == 2)
+        const bugLength = bugList.length
+        console.log(doneBugs)
+        $('#all_bugs').append(bugLength)
+        $('#on_Bug').append(undoneBugs.length)
+        $('#done_bugs').append(doneBugs.length)
+        localStorage.setItem('resultat', JSON.stringify(data))
+        for (const element of data.result.bug) {
           switch (element.state) {
             case '0':
               appendRow(element)
@@ -71,25 +67,26 @@ $(function () {
           }
         }
       } catch (err) {
-        // console.log(message)
-        // alert('une erreur inattendue')
+        console.log(err)
+        alert('une erreur inattendue')
       }
     },
   })
+
+  // code pour récupérer tous les bugs avec l'état "pas commencé"
   $.ajax({
-    url: `http://greenvelvet.alwaysdata.net/bugTracker/api/list/${token}/0`,
+    url: `http://greenvelvet.alwaysdata.net/bugTracker/api/list/${userToken}/0`,
     type: 'GET',
     success: function (response) {
       function appendRow(element) {
-        let users = localStorage.getItem('resultats')
-        let resultats = JSON.parse(users)
+        let localResultats = localStorage.getItem('resultats')
+        let resultats = JSON.parse(localResultats)
         let allUser = resultats.result.user
-        // console.log(allUser)
+
         const options = `
           <option class="option1" value="0">Pas commencé</option>
           <option class="option2" value="1">En cours</option>
-          <option class="option3" value="2">Terminé</option>
-        `
+          <option class="option3" value="2">Terminé</option>`
         const select = `<select class="liste" onchange="updateBug(this.value, ${element.id})">${options}</select>`
         const deleteButton = `<button type="button" id="delete" onclick="deleteBug(${element.id})" class="btn btn-danger">supprimer</button>`
         const row = `
@@ -153,35 +150,40 @@ $(function () {
 
   /*ajouter */
 
-  let form = $('#bug-form')
-  form.on('submit', function (event) {
-    event.preventDefault()
+  // Gestion de l'envoi du formulaire
+let bugForm = $('#bug-form');
+bugForm.on('submit', function (event) {
+  event.preventDefault(); // Empêche le formulaire de se soumettre normalement
 
-    let title = $('#title').val()
-    let description = $('#description').val()
+  // Récupération des données du formulaire
+  let titleInput = $('#title').val();
+  let descriptionInput = $('#description').val();
 
-    let localuser = localStorage.getItem('result')
-    let result = JSON.parse(localuser).result
+  // Récupération des données utilisateur stockées en local
+  let localUser = localStorage.getItem('result');
+  let parsedUser = JSON.parse(localUser).result;
 
-    let token = result.token
-    let user_id = result.id
+  // Extraction du token et de l'ID utilisateur
+  let token = parsedUser.token;
+  let userId = parsedUser.id;
 
-    $.ajax({
-      url: `http://greenvelvet.alwaysdata.net/bugTracker/api/add/${token}/${user_id}`,
-      method: 'POST',
+  // Envoi de la requête AJAX pour ajouter le bug
+  $.ajax({
+    url: `http://greenvelvet.alwaysdata.net/bugTracker/api/add/${token}/${userId}`,
+    method: 'POST',
 
-      data: JSON.stringify({
-        title: title,
-        description: description,
-      }),
-      success: function (response) {
-        console.log(response)
-      },
-      error: function (error) {
-        console.log(error)
-      },
-    })
-  })
+    data: JSON.stringify({
+      title: titleInput,
+      description: descriptionInput,
+    }),
+    success: function (response) {
+      console.log(response);
+    },
+    error: function (error) {
+      console.log(error);
+    },
+  });
+});
 })
 
 /*update */
@@ -209,6 +211,69 @@ function updateBug(state, bug_id) {
       console.log(errorThrown)
     },
   })
+}
+// Gestion de l'envoi du formulaire
+let bugForm = $('#bug-form');
+bugForm.on('submit', function (event) {
+  event.preventDefault(); // Empêche le formulaire de se soumettre normalement
+
+  // Récupération des données du formulaire
+  let titleInput = $('#title').val();
+  let descriptionInput = $('#description').val();
+
+  // Récupération des données utilisateur stockées en local
+  let localUser = localStorage.getItem('result');
+  let parsedUser = JSON.parse(localUser).result;
+
+  // Extraction du token et de l'ID utilisateur
+  let token = parsedUser.token;
+  let userId = parsedUser.id;
+
+  // Envoi de la requête AJAX pour ajouter le bug
+  $.ajax({
+    url: `http://greenvelvet.alwaysdata.net/bugTracker/api/add/${token}/${userId}`,
+    method: 'POST',
+
+    data: JSON.stringify({
+      title: titleInput,
+      description: descriptionInput,
+    }),
+    success: function (response) {
+      console.log(response);
+    },
+    error: function (error) {
+      console.log(error);
+    },
+  });
+});
+
+// Fonction pour mettre à jour l'état d'un bug
+function updateBug(state, bugId) {
+  // Récupération des données utilisateur stockées en local
+  let localUser = localStorage.getItem('result');
+  let parsedUser = JSON.parse(localUser).result;
+
+  // Récupération des données du bug stockées en local
+  let localBug = localStorage.getItem('resultats');
+  let parsedBug = JSON.parse(localBug).result.bug;
+  let token = parsedUser.token;
+
+  // Envoi de la requête AJAX pour mettre à jour l'état du bug
+  $.ajax({
+    url: `http://greenvelvet.alwaysdata.net/bugTracker/api/state/${token}/${bugId}/${state}`,
+    type: 'GET',
+    success: function (response) {
+      console.log(response);
+      // actualise l'affichage de la liste des bugs
+      location.reload();
+      // $("#thead-dark1").empty();
+    },
+    error: function (xhr, textStatus, errorThrown) {
+      console.log(xhr.responseText);
+      console.log(textStatus);
+      console.log(errorThrown);
+    },
+  });
 }
 
 //bouton retour
